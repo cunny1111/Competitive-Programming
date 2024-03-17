0214fe047f1cc6e10f6f649a46906726dee2344a
zawa <97739282+cunny1111@users.noreply.github.com>
2024-03-18 00:13:44 +0700

    Create 71A - Way Too Long Words.cpp

---
+++ Codeforces/Problemsets/71A - Way Too Long Words.cpp
@@ -0,0 +1,29 @@
+#pragma GCC optimize("O3")
+// #pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+// #define int long long
+#define ligma(i, n) for (int i = 0; i < (n); i++)
+#define souls(i, a, b) for (int i = (a); i <= (b); i++)
+#define despairs(i, a, b) for (int i = (a); i >= (b); i--)
+#define all(x) (x).begin(), (x).end()
+#define ___Zawajiro___ signed main()
+#define bliss return 0
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define file(name) if (fopen(name".inp", "r")) { freopen(name".inp", "r", stdin); freopen(name".out", "w", stdout); }
+using namespace std;
+ 
+___Zawajiro___ {
+    fastio;
+    int n;
+    cin >> n;
+ 
+    ligma(i, n) {
+        string s;
+        cin >> s;
+        
+        if (s.length() > 10) cout << s[0] << s.length() - 2 << s[s.length() - 1] << "\n";
+        else cout << s << "\n";
+    }
+    
+    bliss;
+}
