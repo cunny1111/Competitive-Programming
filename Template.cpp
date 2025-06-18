a249b32b85bcdac93de8978e932a97d48372d16c
Cuong Vu <zacksama69@gmail.com>
2025-06-18 18:47:15 +0700

    Update Template.cpp and other files

--- Template.cpp
+++ Template.cpp
@@ -2,19 +2,19 @@
 // #pragma GCC target("avx,avx2,fma")
 #include <bits/stdc++.h>
 // #define int long long
-#define ligma(i, n) for (int i = 0; i < (n); i++)
-#define souls(i, a, b) for (int i = (a); i <= (b); i++)
-#define despairs(i, a, b) for (int i = (a); i >= (b); i--)
-#define all(x) (x).begin(), (x).end()
-#define ___Zawajiro___ signed main()
+#define ____Miyuki____ signed main()
 #define bliss return 0
-#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
 #define file(name) if (fopen(name".inp", "r")) { freopen(name".inp", "r", stdin); freopen(name".out", "w", stdout); }
 using namespace std;
 
-___Zawajiro___ {
+____Miyuki____ {
 
-    // your stupid code here
+    ios_base::sync_with_stdio(false); 
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    // your stupid codes here
 
     bliss;
 }
+
