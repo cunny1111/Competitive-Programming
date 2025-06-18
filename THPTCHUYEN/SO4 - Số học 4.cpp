1ca828dcf6d019c4db2e9190b64c150971e3b66e
cunny1111 <zacksama69@gmail.com>
2025-06-18 21:25:16 +0700

    Added solutions for SO3 and SO4

---
+++ THPTCHUYEN/SO4 - Số học 4.cpp
@@ -0,0 +1,22 @@
+#pragma GCC optimize("O3")
+// #pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#define int long long
+#define ____Miyuki____ signed main()
+#define bliss return 0
+#define file(name) if (fopen(name".inp", "r")) { freopen(name".inp", "r", stdin); freopen(name".out", "w", stdout); }
+using namespace std;
+
+____Miyuki____ {
+
+    ios_base::sync_with_stdio(false); 
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int a, b;
+    cin >> a >> b;
+
+    cout << (a * b) / __gcd(a, b);
+
+    bliss;
+}
