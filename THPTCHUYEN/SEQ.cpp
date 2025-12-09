3c53916013789e5f78bb1d6c62202214fe3ab918
zawajiro <zacksama69@gmail.com>
2025-12-09 08:31:45 +0700

    hh

--- THPTCHUYEN/SEQ.cpp
+++ THPTCHUYEN/SEQ.cpp
@@ -3,14 +3,14 @@
 #define int long long
 using namespace std;
 
-const int inf(LLONG_MAX);
+const int INF(LLONG_MAX);
 
 signed main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  int n, dp[3] = {-inf, -inf, -inf};
+  int n, dp[3] = {-INF, -INF, -INF};
   cin >> n;
 
   vector<int> a(n);
@@ -18,12 +18,13 @@ signed main() {
     cin >> x;
 
   for (int i(0); i < n; i++) {
-    int val = a[i];
-    if (dp[1] != -inf)
-      dp[2] = max(dp[2], dp[1] - val);
+    int val(a[i]);
 
-    if (dp[0] != -inf)
-      dp[1] = max(dp[1], dp[0] + val);
+    if (dp[1] != -INF)
+      dp[2] = max(dp[2], dp[1] + val);
+
+    if (dp[0] != -INF)
+      dp[1] = max(dp[1], dp[0] - val);
 
     dp[0] = max(dp[0], val);
   }
